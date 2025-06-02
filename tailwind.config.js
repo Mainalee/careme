9fe65c5b86293a29281fbd56d3b14feb5111fb5c
[Sadhana Mainali] <[sadie.mainali@gmail.com]>
2025-06-02 19:50:15 +0545

    Fix TailwindCSS PostCSS plugin for Vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // MODIFIED LINE
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
